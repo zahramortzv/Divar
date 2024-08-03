5eb348ba2b522846abe533642ff2dabee2135f33
zahra mortazavi <zahramortazavi23@gmail.com>
2024-08-03 22:37:00 +0330

    fix router path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import Router from "./router/router";
+import Router from "./router/Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { defaultOptions } from "./configs/reactQuery";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
