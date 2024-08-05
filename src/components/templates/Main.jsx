c025eb560dbf5b09c487bc16fb0ab61d9847b662
zahra mortazavi <zahramortazavi23@gmail.com>
2024-08-05 11:59:52 +0330

    fix cannot read "title" bug

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -2,16 +2,17 @@ import { sp } from "../../utils/numbers";
 import styles from "./Main.module.css";
 
 function Main({ posts }) {
+    console.log("posts", posts?.data.posts)
     return (
         <div className={styles.container}>
             {
                 posts?.data.posts.map(post => (
                     <div key={post._id} className={styles.card}>
                         <div className={styles.info}>
-                            <p>{post.options.title}</p>
+                            <p>{post.options?.title}</p>
                             <div>
                                 <p>{sp(post.amount)} تومان</p>
-                                <span>{post.options.city}</span>
+                                <span>{post.options?.city}</span>
                             </div>
                         </div>
                         <img src={`${import.meta.env.VITE_BASE_URL}${post.images[0]}`} />
